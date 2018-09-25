0e38bc030132c829eee861e274df4c08c70997d6
jteng <akariass@gmail.com>
2018-09-25 11:31:21 -0400

    finished implementation of client code without SSL

--- client.py
+++ client.py
@@ -6,47 +6,72 @@ Authors: Jason Teng
 This file contains the server code for CS 5700 Project 1
 """
 
-import asyncore, socket
+import socket, select
 
+# basic validation of start and end of message
+def isValid(message):
+    return message.startswith('cs5700fall2018 ') and message.endswith('\n')
+    
+# returns numeric result of STATUS message
+def solve(message):
+    ops = message.split()
+    if len(ops) < 5:
+        assert 'invalid message'
+        exit()
+    firstnum = int(ops[2])
+    operator = ops[3]
+    secondnum = int(ops[4])
+    
+    if operator == '+':
+        return firstnum + secondnum
+    if operator == '-':
+        return firstnum - secondnum
+    if operator == '*':
+        return firstnum * secondnum
+    if operator == '/':
+        return firstnum // secondnum
+    
+    
+# message handler
+def handleMessage(message, s):
+    if not isValid(message):
+        assert 'invalid message'
+        exit()
+    elif message.find('STATUS') >= 0:
+        result = solve(message)
+        print(result)
+        s.send("cs5700fall2018 " + str(result) + "\n")
+        return
+    elif message.find('BYE') >= 0:
+        secret_flag = message.split()[1]
+        print(secret_flag)
+        s.close()
+        exit()
+        
+
+# Connection Setup
 TCP_HOSTNAME = "cs5700f18.ccs.neu.edu"
 TCP_PORT = 27993
 
+BUFFER_SIZE = 1024
 NUID = "001876580"
 
 INITIAL_MESSAGE = "cs5700fall2018 HELLO " + NUID + "\n" 
 
-class TCPClient(asyncore.dispatcher):
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_HOSTNAME, TCP_PORT))
+s.send(INITIAL_MESSAGE)
 
-    BUFFER_SIZE = 1024
-
-    def __init__(self, address):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect(address)
-        print "connected"
-        self.send(INITIAL_MESSAGE)
-        print "sent"
-
-    def handle_connect(self):
-        pass
-
-    def handle_close(self):
-        self.close()
-        
-    def readable(self):
-        pass
-
-    def handle_read(self):
-        print "can read"
-        self.data = self.recv(BUFFER_SIZE)
-        print self.data
-
-    def writable(self):
-        pass
-
-    def handle_write(self):
-        pass
-
-
-client = TCPClient((TCP_HOSTNAME, TCP_PORT))
-asyncore.loop()
+# Event loop
+while True:
+    try:
+        readable, writable, errored = select.select([s,], [], [], 3)
+    except select.error:
+        s.close()
+        exit()
+    if len(readable) > 0:
+        message = s.recv(2048)
+        print(message)
+        handleMessage(message, s)
+    else:
+        print 'timeout'
